bbb6c507a63e7b7c49b72e667a8692313d044022
AustenSorochak <austensorochak@gmail.com>
2022-03-09 15:05:35 -0800

    define the data model and initialize the DB

--- msgs/models.py
+++ msgs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinLengthValidator
 
 # Create your models here.
+
+class Msg(models.Model):
+  name = models.CharField(max_length=15, validators=[
+    RegexValidator('^[A-Za-z]+$'),
+    MinLengthValidator(2)
+  ])
+  msgText = models.CharField(max_length=30, validators=[
+    MinLengthValidator(3)
+  ])
+  
+  class Meta:
+    ordering = ['-id']
+
