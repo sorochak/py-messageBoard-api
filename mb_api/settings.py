d276072dd0dffdfa00e075b2781099354a74658d
AustenSorochak <austensorochak@gmail.com>
2022-03-10 12:17:36 -0800

    add CORS

--- mb_api/settings.py
+++ mb_api/settings.py
@@ -39,11 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party Apps
     'rest_framework',
+    'corsheaders',
+
     # Local App
     'msgs.apps.MsgsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'mb_api.urls'
 
